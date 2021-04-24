167d725bde31cc16717338850ba63442785be4dd
Krystian Wróbel <wrobelkrystian@gazeta.pl>
2021-04-24 18:36:55 +0200

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import '../scss/main.scss';
 
+import moment from 'moment';
+
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
 // registerSW();
@@ -7,3 +9,24 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+const button = document.querySelector('.action--js');
+
+console.log(button)
+
+
+button.addEventListener('click', ()=> {
+  const heading = document.querySelector(".main__heading--js");
+  heading.innerHTML = `Zerżnąć cię?`
+});
+
+const menu = document.querySelector('.menu--js');
+
+
+
+
+
+const startOfDay = moment().startOf('day').fromNow();
+
+const timePlaceholder = document.querySelector('.time--js');
+
+timePlaceholder.innerHTML = startOfDay;
